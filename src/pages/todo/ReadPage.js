0efd31a707b6eadff64b1576b8264ae565bcf1c1
realdeer <i3295h@naver.com>
2025-01-20 01:12:03 +0900

    숙제 모두 완료

--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,27 +1,20 @@
-import { useParams, useNavigate, useSearchParams, createSearchParams } from "react-router-dom";
+import {
+  useParams,
+  useNavigate,
+  useSearchParams,
+  createSearchParams,
+} from "react-router-dom";
 import { useCallback } from "react";
 import ReadComponent from "../../components/todo/ReadComponent";
 
 const ReadPage = () => {
   const { tno } = useParams();
-  const navigate = useNavigate();
-
-  const moveToModify = useCallback(
-    (tno) => {
-      navigate({ pathname: `/todo/modify/${tno}` });
-    },
-    [tno]
-  );
 
   return (
     <div className="font extrabold w-full bg-white mt-6">
-    <div className="text-2xl">
-        Todo Read Page Component {tno}
+      <div className="text-2xl">Todo Read Page Component {tno}</div>
+      <ReadComponent tno={tno}></ReadComponent>
     </div>
-    <ReadComponent tno={tno}></ReadComponent>
-</div>
-
-
   );
 };
 
