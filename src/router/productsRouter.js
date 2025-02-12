be8910d1671d89e4c3245d0ed4287c9234d766f0
realdeer <i3295h@naver.com>
2025-02-12 14:49:19 +0900

    글 수정하기

--- src/router/productsRouter.js
+++ src/router/productsRouter.js
@@ -2,21 +2,41 @@ import { lazy, Suspense } from "react";
 import { Navigate } from "react-router-dom";
 
 const productsRouter = () => {
-    const Loading = <div>Loading.....</div>
+  const Loading = <div>Loading.....</div>;
 
-    const ProductsList = lazy(() => import("../pages/products/ListPage"));
-    const ProductsADD = lazy(() => import("../pages/products/AddPage"));
-    return[
-        {
-                path: "list", element: <Suspense fallback={Loading}><ProductsList></ProductsList></Suspense>
-        },
-        {
-            path:"", element:<Navigate replace to = "/products/list"></Navigate>
-        },
-        {
-            path: "add", element: <Suspense fallback={Loading}><ProductsADD></ProductsADD></Suspense>
-    }
-    ]
-}
+  const ProductsList = lazy(() => import("../pages/products/ListPage"));
+  const ProductsADD = lazy(() => import("../pages/products/AddPage"));
+  const ProductsRead = lazy(() => import("../pages/products/ReadPage"));
+  return [
+    {
+      path: "list",
+      element: (
+        <Suspense fallback={Loading}>
+          <ProductsList></ProductsList>
+        </Suspense>
+      ),
+    },
+    {
+      path: "",
+      element: <Navigate replace to="/products/list"></Navigate>,
+    },
+    {
+      path: "add",
+      element: (
+        <Suspense fallback={Loading}>
+          <ProductsADD></ProductsADD>
+        </Suspense>
+      ),
+    },
+    {
+      path: "read/:pno",
+      element: (
+        <Suspense fallback={Loading}>
+          <ProductsRead></ProductsRead>
+        </Suspense>
+      ),
+    },
+  ];
+};
 
 export default productsRouter;
